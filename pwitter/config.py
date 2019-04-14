4480413e09aedf348eca6ba208a2087826cb5e61
Andrea Corsini <andrea.corsini@outlook.com>
2019-04-14 22:34:15 +0200

    Overwrite config properties by env vars

--- pwitter/config.py
+++ pwitter/config.py
@@ -1,8 +1,9 @@
 import ConfigParser as cp
+import os
 
 CONFIG_FILE = 'pwitter.conf'
 _CONF = cp.ConfigParser()
 _CONF.readfp(open(CONFIG_FILE))
 
 def get(opt, section='DEFAULT'):
-    return _CONF.get(section, opt)
+    return _CONF.get(section, opt, vars=os.environ)
